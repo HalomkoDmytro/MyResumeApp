d1405d7f1fca00659ad3eba07c48c6fece9c0e99
HalomkoDmytro <galomko.dmytro@gmail.com>
2019-11-10 22:41:40 +0200

    refactor/create editProfileController class

--- src/main/java/com/myresume/controller/PublicController.java
+++ src/main/java/com/myresume/controller/PublicController.java
@@ -1,15 +1,19 @@
 package com.myresume.controller;
 
+import com.myresume.dao.ProfileRepository;
+import com.myresume.dao.SkillCategoryRepository;
 import com.myresume.entity.Profile;
 import com.myresume.service.NameService;
-import com.myresume.service.TestService;
+import com.myresume.service.ProfileService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PublicController {
@@ -20,7 +24,13 @@ public class PublicController {
     private NameService nameService;
 
     @Autowired
-    private TestService service;
+    private ProfileService profileService;
+
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    @Autowired
+    private SkillCategoryRepository skillCategoryRepository;
 
     @GetMapping({"/"})
     public String index() {
@@ -32,21 +42,32 @@ public class PublicController {
         return "jsp/profile";
     }
 
+    @GetMapping("/welcome")
+    public String welcome(@RequestParam(required = false) String page, Model model) {
+        final Page<Profile> chunkCompletedProfiles = profileService.getChunkCompletedProfiles(page);
+        model.addAttribute("profiles", chunkCompletedProfiles.getContent());
+        return "jsp/welcome";
+    }
+
     @GetMapping({"/profile/{uid}"})
     public String profileWithName(@PathVariable("uid") String uis, Model model) {
-        final String s = nameService.convertName(uis);
-        model.addAttribute("fullName", s);
-        return "jsp/profile";
-    }
-
-    @GetMapping(value = "/testProfile")
-    public String testProfile(Model model) {
-        final Profile profile = service.profile();
-
+        final Profile profile = profileRepository.findByUid(uis);
+        if (profile == null) {
+            return "jsp/profile-not-found";
+        }
         model.addAttribute("profile", profile);
         model.addAttribute("contacts", profile.getContacts());
-
         return "jsp/profile";
     }
+
+    private String gotoSkillsJSP(Model model) {
+        model.addAttribute("skillCategories", skillCategoryRepository.findAll());
+        return "jsp/edit/skills";
+    }
+
+//    @GetMapping(value = "/test")
+//    public String test(Model model) {
+//        return "jsp/edit/edit-personal-info";
+//    }
 
 }
